0d0cac3a2df5dfdffab3cbf8a5f7d8180440ce23
eliasreyna1 <114760292+eliasreyna1@users.noreply.github.com>
2023-02-27 16:19:42 -0300

    repositorio
    proyecto final

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let progress = document.querySelector('.progress');
+let value = 75; // valor de la barra de progreso
+
+progress.style.width = value + '%';
+
+
+
+
+const toggleNav = document.getElementById('toggle-nav');
+const menu = document.getElementById('menu');
+
+toggleNav.addEventListener('click', function() {
+  menu.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
